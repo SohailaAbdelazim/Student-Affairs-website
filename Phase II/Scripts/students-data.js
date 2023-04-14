5b7115a9f0f155feff0487640ddd86e562cc1f6e
Sohaila Abdelazim Khalifa <sohailakhalifa03@gmail.com>
2023-04-14 20:21:14 +0200

    Fixing all styles - links and main website page .

---
+++ Phase II/Scripts/students-data.js
@@ -0,0 +1,8 @@
+function deleteFunction(){
+document.getElementById("myTable").deleteRow(1);
+}
+
+// function deleteFunction(r) {
+//     var i = r.parentNode.parentNode.rowIndex;
+//     document.getElementById("myTable").deleteRow(i);
+// }
