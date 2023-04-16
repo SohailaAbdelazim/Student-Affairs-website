f7c82e69bb9731e1bc603bf8dafd3738d6293276
Omar <omarkhaled1752003@yahoo.com>
2023-04-16 02:46:45 +0200

    updated login page and homepage

--- FINAL/js/students-data.js
+++ FINAL/js/students-data.js
@@ -6,3 +6,25 @@ document.getElementById("myTable").deleteRow(1);
 //     var i = r.parentNode.parentNode.rowIndex;
 //     document.getElementById("myTable").deleteRow(i);
 // }
+
+/*
+    scripts for navigation bar
+*/
+
+function dropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+  // Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
---
+++ Phase II/JS/javafun.js
@@ -0,0 +1,1 @@
+/* This is the main java script file for our project */ 
