521ec87dec72fce00049db16825539ca56871b62
Mohamed Ahmed Abdelghany <77937077+abdelghany-74@users.noreply.github.com>
2023-04-17 01:05:11 +0200

    store the data in the local storage

--- FINAL/js/students-data.js
+++ FINAL/js/students-data.js
@@ -28,3 +28,25 @@ window.onclick = function(event) {
         }
     }
 }
+
+
+// function to store the data in the local storage .
+let studentArray = localStorage.getItem("students")
+  ? JSON.parse(localStorage.getItem("students"))
+  : [];
+function addStudentToLocalStorage() {
+  student = {
+    id: document.getElementById("studentId").value,
+    name: document.getElementById("studentName").value,
+    date: document.getElementById("studentDate").value,
+    gpa: document.getElementById("studentGpa").value,
+    gender: document.getElementById("studentGender").value,
+    level: document.getElementById("studentLevel").value,
+    status: document.getElementById("studentStatus").value,
+    department: document.getElementById("studentDep").value,
+    email: document.getElementById("studentEmail").value,
+    phone: document.getElementById("studentPhone").value,
+  };
+  studentArray.push(student);
+  localStorage.setItem("students", JSON.stringify(studentArray));
+}
