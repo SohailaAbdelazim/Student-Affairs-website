5f292a2e3f62d3f6996a0d34dfa3dcc5a381782b
Sohaila Abdelazim Khalifa <sohailakhalifa03@gmail.com>
2023-04-17 23:05:50 +0200

    sohaila edit func

--- FINAL/js/students-data.js
+++ FINAL/js/students-data.js
@@ -65,6 +65,88 @@ function deleteconfirmation() {
 } 
 
 
-function editstudent(){
+/*function editstudent(){
   document.getElementById("").value= student.name; 
-}
+}*/ 
+
+function editdata(id) {
+  // Find the student with the matching id
+  let studentArray = localStorage.getItem("students")
+  ? JSON.parse(localStorage.getItem("students"))
+  : [];
+ // var students = JSON.parse(localStorage.getItem("students")) || [];
+  var studentIndex = studentArray.findIndex(function(student) {
+    return student.id === id;
+  });
+  var student = studentArray[studentIndex];
+  console.log(student); 
+  student.name='sohaila'; 
+  document.querySelector('#studentName').value = student.name ; 
+
+ /* // Fill the edit form with the student's information
+  document.getElementById("studentId").innerHTML = student.id;
+  document.getElementById("studentName").innerHTML = student.name;
+  document.getElementById("studentDate").value = student.dob;
+  document.getElementById("studentGpa").value = student.gpa;
+  document.getElementById("studentGender").value = student.gender;
+  document.getElementById("studentLevel").value = student.level;
+  document.getElementById("studentStatus").value = student.status;
+  document.getElementById("studentEmail").value = student.email;
+  document.getElementById("studentPhone").value = student.phone; */
+
+  // Save the updated student data to local storage
+  document.getElementById("saveEditButton").addEventListener("click", function() {
+    student.id = document.getElementById("studentId").value;
+    student.name = document.getElementById("studentName").value;
+    student.dob = document.getElementById("studentDate").value;
+    student.gpa = document.getElementById("studentGpa").value;
+    student.gender = document.getElementById("studentGender").value;
+    student.level = document.getElementById("studentLevel").value;
+    student.status = document.getElementById("studentStatus").value;
+    student.email = document.getElementById("studentEmail").value;
+    student.phone = document.getElementById("studentPhone").value;
+
+    localStorage.setItem("students", JSON.stringify(students));
+  });
+} 
+
+
+// get the Save button
+const saveBtn = document.querySelector('.save');
+
+// add event listener to the Save button
+saveBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  // get the input values
+  const name = document.querySelector('#studentName').value;
+  const birthdate = document.querySelector('#studentDate').value;
+  const gpa = document.querySelector('#studentGpa').value;
+  const gender = document.querySelector('input[name="ma"]:checked').value;
+  const level = document.querySelector('select[name="level"]').value;
+  const status = document.querySelector('input[name="BE"]:checked').value;
+  const email = document.querySelector('#studentEmail').value;
+  const tel = document.querySelector('#studentPhone').value;
+
+  // get the student data from the local storage
+  let studentsData = JSON.parse(localStorage.getItem('studentsData'));
+
+  // find the student with the same name in the array and update their data
+  studentsData.forEach((student, index) => {
+    if (student.name === name) {
+      student.birthdate = birthdate;
+      student.gpa = gpa;
+      student.gender = gender;
+      student.level = level;
+      student.status = status;
+      student.email = email;
+      student.tel = tel;
+    }
+  });
+
+  // save the updated student data back to the local storage
+  localStorage.setItem('studentsData', JSON.stringify(studentsData));
+
+  // redirect to the students data page
+  window.location.href = 'students-data.html';
+});
